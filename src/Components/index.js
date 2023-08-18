cfeb717ce01d8a33f0aea2ee949f9263148391b4
Rishiraj <Rishirajshinde1610@gmail.com>
2023-08-18 23:18:51 +0530

    first commit

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import Button from './Button';
+import PopularProductCard from "./PopularProductCard";
+import Review from "./Review";
+import Service from "./Service";
+
+export{Navbar,Button,PopularProductCard,Review,Service};
